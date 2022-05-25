41146e507d933896024d66716f7e9b9253c86431
SirXYZ <92049410+SirXYZ@users.noreply.github.com>
2022-05-25 23:18:29 +0200

    Removed login and now working on re-adding it
    :)

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -9,20 +9,62 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class BaseController extends AbstractController
 {
     /**
-     * @Route("/home")
+     * @Route("/", name="home")
      */
     public function setVariables(): Response{
-        $articles = [
-            '0' => ['title' => 'Frühling', 'body' => 'Der Frühling beginnt...'],
-            '1' => ['title' => 'Sommer', 'body' => 'Der Sommer ist so...'],
-            '2' => ['title' => 'Herbst', 'body' => 'Der Herbst ist immer...'],
-            '3' => ['title' => 'Winter', 'body' => 'Der Winter war...']
-        ];
+        $darkMode = false;
+        if(isset($_COOKIE["darkMode"])) {
+            $darkMode = $_COOKIE["darkMode"];
+        }
+
         return $this->render('base.html.twig', [
-            'articles' => $articles,
-            'darkMode' => true,
+            'darkMode' => $darkMode,
             'logged_in' => false,
             'unread_messages' => 0
         ]);
     }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function setVariables2(): Response{
+
+        return $this->render('/wikiPages/home.html.twig', [
+
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/darkMode", name="setDarkMode")
+     */
+    public function setDarkMode(): Response{
+        # Setzt einen Cookie mit setcookie(name, value, expire, path, domain, secure, httponly);
+        # name ist der Name des Cookies und mit diesem interagiert man auch mit ihm, value ist der Wert, expire das Verfallsdatum, path ist auf welchen Seiten der Cookie funktionieren soll
+        setcookie("darkMode", true, time() + (86400 * 30), "/");
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/lightMode", name="setLightMode")
+     */
+    public function setLightMode(): Response{
+        setcookie("darkMode", false, time() + (86400 * 30), "/");
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/rdmWiki", name="generateNumber")
+     */
+    public function generateNumber(): Response{
+        try {
+            $number = random_int(0, 100);
+        } catch (\Exception $e) {
+            $number = 1;
+        }
+        return $this->redirectToRoute('home');
+    }
+
+
 }
