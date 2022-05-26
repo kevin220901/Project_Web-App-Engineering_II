9c81513d431aefb248b1fd31b99e625e6f49a77a
SirXYZ <92049410+SirXYZ@users.noreply.github.com>
2022-05-26 20:12:36 +0200

    Added md Parser and

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,10 @@ if(darkModeButton !== null){
         const d = new Date();
         d.setTime(d.getTime() + (2*24*60*60*1000));
         let expires = "expires="+ d.toUTCString();
+        // Setzt einen Cookie mit dem Namen darkMode und der Value "true" als String !, path=/ sagt, dass der Cookie auf der ganzen Seite funktioniert
+        // und SameSite=Strict sagt, dass der Cookie nur im Kontext dieser Seite gesetzt wird
         document.cookie = "darkMode" + "=" + true + ";" + expires + ";path=/; SameSite=Strict;";
+        // Jetzt wird die Seite neu geladen, damit die css Klassen überschrieben werden können
         location.reload();
         return false;
     }
@@ -29,6 +32,7 @@ const registerButton = document.getElementById("pill-register-button");
 if(loginButton !== null){
     if(registerButton !== null){
         loginButton.onclick = function(event) {
+            // Der if Block ist nur für dark/light mode und überprüft welcher btn stlye genutzt wird
             if($('#pill-login-button').hasClass('btn-light')){
                 $('#pill-login-button').removeClass('btn-light');
                 $('#pill-register-button').addClass('btn-light');
@@ -65,3 +69,54 @@ if(loginButton !== null){
         }
     }
 }
+// Parser zu .md
+//let text='# Ich bin eine Überschrift\n- Und ich bin ein Punkt! ```<p class="text-primary"><script>alert("XSS")</script></p>```<img src="x"/>'
+let text='Marked - Markdown Parser\n' +
+    '========================\n' +
+    '\n' +
+    '[Marked] lets you convert [Markdown] into HTML.  Markdown is a simple text format whose goal is to be very easy to read and write, even when not converted to HTML.  This demo page will let you type anything you like and see how it gets converted.  Live.  No more waiting around.\n' +
+    '\n' +
+    'How To Use The Demo\n' +
+    '-------------------\n' +
+    '\n' +
+    '1. Type in stuff on the left.\n' +
+    '2. See the live updates on the right.\n' +
+    '\n' +
+    'That\'s it.  Pretty simple.  There\'s also a drop-down option in the upper right to switch between various views:\n' +
+    '\n' +
+    '- **Preview:**  A live display of the generated HTML as it would render in a browser.\n' +
+    '- **HTML Source:**  The generated HTML before your browser makes it pretty.\n' +
+    '- **Lexer Data:**  What [marked] uses internally, in case you like gory stuff like this.\n' +
+    '- **Quick Reference:**  A brief run-down of how to format things using markdown.\n' +
+    '\n' +
+    'Why Markdown?\n' +
+    '-------------\n' +
+    '\n' +
+    'It\'s easy.  It\'s not overly bloated, unlike HTML.  Also, as the creator of [markdown] says,\n' +
+    '\n' +
+    '> The overriding design goal for Markdown\'s\n' +
+    '> formatting syntax is to make it as readable\n' +
+    '> as possible. The idea is that a\n' +
+    '> Markdown-formatted document should be\n' +
+    '> publishable as-is, as plain text, without\n' +
+    '> looking like it\'s been marked up with tags\n' +
+    '> or formatting instructions.\n' +
+    '\n' +
+    'Ready to start writing?  Either start changing stuff on the left or\n' +
+    '[clear everything](/demo/?text=) with a simple click.\n' +
+    '\n' +
+    '[Marked]: https://github.com/markedjs/marked/\n' +
+    '[Markdown]: http://daringfireball.net/projects/markdown/\n' +
+    '\n' +
+    '```C\n' +
+    'Scanf("%s",&H);\n' +
+    'assdasd\n' +
+    'asddddadaadada\n' +
+    '```\n' +
+    '`Test`'
+let dirtyText = marked.parse(text); //parsed md zu html
+// Erlaubt nur HTML
+let cleanText = DOMPurify.sanitize(dirtyText, { USE_PROFILES: {html: true} }); //Überprüft den Code und entfernt alle Text die nicht in standart html sind <script> etc
+// Erlaubt HTML, SVG und MathML
+//let cleanText = DOMPurify.sanitize(dirtyText);
+$("#parseToMD").html(cleanText);
--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -14,41 +14,32 @@ class BaseController extends AbstractController
         if(isset($_COOKIE["darkMode"])) {
             $darkMode = $_COOKIE["darkMode"];
         }
+        // Da Js true als String speichert muss man den string in einen boolean umformen
         return filter_var($darkMode, FILTER_VALIDATE_BOOLEAN);
     }
-
 
     /**
      * @Route("/", name="home")
      */
     public function renderBase(): Response{
 
-        return $this->render('base.html.twig', [
+        return $this->render('/wikiPages/home.html.twig', [
             'darkMode' => $this->getDarkMode()
         ]);
     }
 
-    /**
-     * @Route("/test", name="test")
-     */
-    public function setVariables2(): Response{
-
-        return $this->render('/wikiPages/home.html.twig', [
-            'darkMode' => $this->getDarkMode(),
-        ]);
-    }
 
     /**
      * @Route("/wiki/{id}", name="wiki")
      */
     public function renderWiki(int $id): Response{
-        return $this->render('/wikiPages/home.html.twig', [
+        return $this->render('/wikiPages/wikiPage.html.twig', [
             'darkMode' => $this->getDarkMode(),
             "WikiID" => $id
         ]);
     }
 
-    // Generates a rdm number and gives it to the /wiki/{id} route
+    // Erzeugt eine rdm Nummer und gibt diese an die wiki Route
     /**
      * @Route("/rdmWiki", name="generateNumber")
      */
@@ -58,14 +49,24 @@ class BaseController extends AbstractController
         } catch (\Exception $e) {
             $number = 1;
         }
+        // geht an die Wiki Route, da die Route eine Variable {id} hat muss ein array der Variablen ebenfalls übergeben werden
         return $this->redirectToRoute('wiki', array('id' => $number));
     }
+
     /**
      * @Route("/login", name="login")
      */
     public function renderLogin(): Response{
-
         return $this->render('/wikiPages/login.html.twig', [
+            'darkMode' => $this->getDarkMode(),
+        ]);
+    }
+
+    /**
+     * @Route("/browse", name="browse")
+     */
+    public function renderBrowse(): Response{
+        return $this->render('/wikiPages/browse.html.twig', [
             'darkMode' => $this->getDarkMode(),
         ]);
     }
